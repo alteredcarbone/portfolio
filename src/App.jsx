8a5c3fa6ba70ec791330477fe3e1a64f86ada7db
alteredcarbone <maximedupont464646@gmail.com>
2026-03-11 09:46:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BroswerRouter, Routes, Route} from "react-router-dom";
+
+import './App.css'
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" component={} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+
